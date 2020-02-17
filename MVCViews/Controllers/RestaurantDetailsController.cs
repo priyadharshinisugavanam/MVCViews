9d01e40ea985a7e7014844dc00c5d90ed4031a33
priyadharshinisugavanam <priyadharshinisugavanam@gmail.com>
2020-02-17 19:09:07 +0530

    commit

--- MVCViews/Controllers/RestaurantDetailsController.cs
+++ MVCViews/Controllers/RestaurantDetailsController.cs
@@ -1,21 +1,81 @@
-﻿using System;
+﻿using Restaurant.DAL;
+using Restaurant.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVCViews.Controllers
+namespace MVCView.Controllers
 {
     public class RestaurantDetailsController : Controller
     {
         // GET: RestaurantDetails
-        public ActionResult Index()
+        RestaurantRepositary restaurantRepositary = new RestaurantRepositary();
+        public ViewResult Index()
+        {
+
+            IEnumerable<RestaurantEntity> restaurants = restaurantRepositary.DisplayRestaurant();
+            return View(restaurants);
+        }
+        public ViewResult RestaurantDetailsDisplay()
+        {
+
+            IEnumerable<RestaurantEntity> restaurants = restaurantRepositary.DisplayRestaurant();
+            ViewBag.RestaurantDetails = restaurants;
+            return View();
+        }
+
+        public ActionResult ViewDetails()
+        {
+            RestaurantRepositary restaurantRepositary = new RestaurantRepositary();
+            IEnumerable<RestaurantEntity> restaurants = restaurantRepositary.DisplayRestaurant();
+            ViewData["RestaurantDetails"] = restaurants;
+            return View();
+        }
+        public ActionResult ViewAllDetails()
+        {
+            IEnumerable<RestaurantEntity> restaurants = restaurantRepositary.DisplayRestaurant();
+            TempData["Restaurant"] = restaurants;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
         {
             return View();
-
-
-
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult CreateUpdate()
+        {
+            RestaurantEntity restaurantEntity = new RestaurantEntity();
+            TryUpdateModel(restaurantEntity);
+            restaurantRepositary.AddRestaurant(restaurantEntity);
+            TempData["Message"] = "Restaurant Added";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string restaurantName)
+        {
+            RestaurantEntity restaurant = restaurantRepositary.GetRestaurant(restaurantName);
+            return View(restaurant);
+        }
+        public ActionResult Delete(string restaurantName)
+        {
+            restaurantRepositary.DeleteRestaurant(restaurantName);
+            TempData["Message"] = "Restaurant deleted";
+            return RedirectToAction("Index");
 
         }
+        [HttpPost]
+        [ActionName("Update")]
+        public ActionResult UpdateTry()
+        {
+            RestaurantEntity restaurantEntity = new RestaurantEntity();
+            UpdateModel(restaurantEntity);
+            restaurantRepositary.UpdateRestaurant(restaurantEntity);
+            TempData["Message"] = "Restaurant Updated";
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ Restaurant.DAL/RestaurantRepositary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurant.Entity;
+
+namespace Restaurant.DAL
+{
+    public class RestaurantRepositary
+    {
+        static List<RestaurantEntity> restaurantEntities = new List<RestaurantEntity>();
+
+        static RestaurantRepositary()
+        {
+            RestaurantEntity restaurantEntity = new RestaurantEntity("Chennise", "Indian", "Sarada college,Salem");
+            restaurantEntities.Add(restaurantEntity);
+            restaurantEntity = new RestaurantEntity("Taj", "Chinese", "Bus-Stand,Erode");
+            restaurantEntities.Add(restaurantEntity);
+        }
+        public IEnumerable<RestaurantEntity> DisplayRestaurant()
+        {
+            return restaurantEntities;
+        }
+        public void AddRestaurant(RestaurantEntity restaurantEntity)
+        {
+            restaurantEntities.Add(restaurantEntity);
+        }
+        public RestaurantEntity GetRestaurant(string restaurantName)
+        {
+            return restaurantEntities.Find(id => id.RestaurantName == restaurantName);
+        }
+        public void DeleteRestaurant(string restaurantName)
+        {
+            RestaurantEntity restaurant = GetRestaurant(restaurantName);
+            if (restaurant != null)
+            {
+                restaurantEntities.Remove(restaurant);
+            }
+        }
+        public void UpdateRestaurant(RestaurantEntity entity)
+        {
+            RestaurantEntity restaurantEntity = GetRestaurant(entity.RestaurantName);
+            restaurantEntity.RestaurantType = entity.RestaurantType;
+            restaurantEntity.Location = entity.Location;
+        }
+    }
+}
