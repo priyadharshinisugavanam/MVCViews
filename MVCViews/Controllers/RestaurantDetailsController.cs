3aa8ebdfcc24579efc172badb74b3d486da309c3
priyadharshinisugavanam <priyadharshinisugavanam@gmail.com>
2020-02-17 18:54:35 +0530

    Add project files.

---
+++ MVCViews/Controllers/RestaurantDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCViews.Controllers
+{
+    public class RestaurantDetailsController : Controller
+    {
+        // GET: RestaurantDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
